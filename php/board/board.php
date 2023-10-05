4db826d7c00f3d0296b095ecb7f85f5faf6acd09
jade1087z <danziro57@gmail.com>
2023-10-05 17:59:32 +0900

    2023.10.05

--- php/board/board.php
+++ php/board/board.php
@@ -5,6 +5,15 @@
     // echo"<pre>";
     // var_dump($_SESSION);
     // echo"</pre>";
+
+    // 총 페이지 갯수
+    $sql = "SELECT count(boardID) FROM board";
+    $result = $connect -> query($sql);
+
+    $boardTotalCount = $result -> fetch_array(MYSQLI_ASSOC);
+    $boardTotalCount = $boardTotalCount['count(boardID)'];
+
+    // echo $boardTotalCount;
 ?>
 
 <!DOCTYPE html>
@@ -37,6 +46,26 @@
             </div>
         </div>
         <section class="board__inner container">
+            <div class="board__search">
+                <div class="left">
+                    * 총<em><?=$boardTotalCount?></em>건의 게시물이 등록되어 있습니다.
+                </div>
+                <div class="right">
+                    <form action="boardSearch.php" name="boardSearch" method="post">
+                        <fieldset>
+                            <legend class="blind">게시판 검색 영역</legend>
+                        </fieldset>
+                        <input type="search" name="searchKeyword" id="searchKeyword" placeholder="검색어를 입력하세요!" required>
+                        <select name="searchOption" id="searchOption">
+                            <option value="title">제목</option>
+                            <option value="content">내용</option>
+                            <option value="name">등록자</option>
+                        </select>
+                        <button type="submit">검색</button>
+                        <a href="boardWrite.php">글쓰기</a>
+                    </form>
+                </div>
+            </div>
             <div class="board__table">
                 <table>
                     <colgroup>
@@ -56,15 +85,103 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
+                        <!-- <tr>
                             <td>1</td>
                             <td><a href="boardView">게시판 제목</a></td>
                             <td>yys</td>
                             <td>2023-10-04</td>
                             <td>100</td>
-                        </tr>
+                        </tr> -->
+                        <?php
+                            if(isset($_GET['page'])){
+                                $page = (int) $_GET['page'];
+                            } else {
+                                $page = 1;
+                            }
+
+                            $viewNum = 10;
+                            $viewLimit = ($viewNum * $page) - $viewNum;
+                            // 1~10 LIMIT 0, 10 --> page1 ($viewNum * 1) - ($viewNum)
+                            // 11~20 LIMIT 10, 10 --> page2 ($viewNum * 2) - ($viewNum *1)
+                            // 21~30 LIMIT 20, 10 --> page3 ($viewNum * 3) - ($viewNum *1)
+
+                            $sql = "SELECT b.boardID, b.boardTitle, m.youName, b.regTime, b.boardView FROM board b JOIN members m ON(b.memberID = m.memberID) ORDER BY boardID DESC LIMIT {$viewLimit}, {$viewNum}";
+                            $result = $connect -> query($sql);
+
+                            if($sql){
+                                $count = $result -> num_rows;
+                                
+                                if($count > 0){
+                                    for($i=0; $i<$count; $i++){
+                                        $info = $result -> fetch_array(MYSQLI_ASSOC);
+
+                                        echo "<tr>";
+                                        echo "<td>".$info['boardID']."</td>";
+                                        echo "<td>".$info['boardTitle']."</td>";
+                                        echo "<td>".$info['youName']."</td>";
+                                        echo "<td>".date('Y-m-d', $info['regTime'])."</td>";
+                                        echo "<td>".$info['boardView']."</td>";
+                                        echo "</tr>";
+                                    }
+                                } else {
+                                    echo "<tr><td colspan='5'>게시글이 없습니다.</td></tr>";
+                                }
+                            } else {
+                                echo "관리자에게 문의하세요.";
+                            }
+                        ?>
                     </tbody>
                 </table>
+            </div>
+            <div class="board__pages">
+                <ul>
+                    <?php
+                        // 총 페이지 갯수
+                        $boardTotalCount = ceil($boardTotalCount/$viewNum);
+
+                        // 현재 보고있는 페이지 앞뒤로 자르기
+                        $pageView = 5;
+                        $startPage = $page - $pageView;
+                        $endPage = $page + $pageView;
+                        
+                        // 처음페이지, 마지막 페이지 초기화
+                        if($startPage < 1) $startPage = 1;
+                        if($endPage >= $boardTotalCount) $endPage = $boardTotalCount;
+                        
+                        // 이전 페이지 링크
+                        if($page > 1){
+                            echo "<li class='first'><a href='board.php?page=1'>처음으로</a></li>";
+                            echo "<li class='prev'><a href='board.php?page=".($page-1)."'>이전</a></li>";
+                        }
+                        
+                        // 페이지
+                        // for($i=$startPage; $i<=$endPage; $i++){
+                        //     $activeClass = ($i == $page) ? 'active' : '';
+                        //     echo " <li class='$activeClass'><a href='#'>${i}</a></li>";
+                        // }
+                        for($i=$startPage; $i<=$endPage; $i++){
+                            $active = "";
+                            if($i == $page) $active = "active";
+                            echo " <li class='{$active}'><a href='board.php?page={$i}'>${i}</a></li>";
+                        }
+                        // 다음 페이지 링크
+                        if($page < $boardTotalCount){
+                            echo "<li class='next'><a href='board.php?page=".($page+1)."'>다음</a></li>";
+                            echo "<li class='last'><a href='board.php?page={$boardTotalCount}'>마지막으로</a></li>";
+                        }
+                    ?>
+                    <!-- <li class="first"><a href="#">처음으로</a></li>
+                    <li class="prev"><a href="#">이전</a></li>
+                    <li class="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">6</a></li>
+                    <li><a href="#">7</a></li>
+                    <li class="next"><a href="#">다음</a></li>
+                    <li class="last"><a href="#">마지막으로</a></li> -->
+                </ul>
             </div>
         </section>
     </main>
--- php/board/boardWriteSave.php
+++ php/board/boardWriteSave.php
@@ -1,19 +1,43 @@
-<?php
-    include "../connect/connect.php";
-    include "../connect/session.php";
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include "../connect/connect.php";
+        include "../connect/session.php";
 
-    $boardTitle = $_POST['boardTitle'];
-    $boardContents = $_POST['boardContents'];
-    $boardView = 1;
-    $regTime = time();
-    $memberID = $_SESSION['memberID'];
+        $boardTitle = $_POST['boardTitle'];
+        $boardContents = $_POST['boardContents'];
+        $boardView = 0;
+        $regTime = time();
+        $memberID = $_SESSION['memberID'];
 
-    // echo $boardTitle, $boardContents, $memberID;
+        // echo $boardTitle, $boardContents, $memberID;
 
-    $boardTitle = $connect -> real_escape_string($boardTitle);
-    $boardContents = $connect -> real_escape_string($boardContents);
-
-    $sql = "INSERT INTO board(memberID, boardTitle, boardContents, boardView, regTime) VALUES('$memberID', '$boardTitle', '$boardContents', '$boardView', '$regTime')";
-
-    $connect -> query($sql);
-?>
+        // 세션을 통해 사용자가 로그인되어 있는지 확인
+        if(!isset($_SESSION['memberID'])){
+            echo "<script>alert('로그인 후에 게시글을 작성할 수 있습니다.');</script>";
+            echo "<script>window.history.back();</script>"; // 이전 페이지로 이동
+        } else {
+            if(empty($boardTitle) || empty($boardContents)){ // 제목이나 콘텐츠 내용이 없으면 등록X
+                echo "<script>alert('제목과 내용을 모두 입력해주세요.');</script>";
+                echo "<script>window.history.back();</script>"; // 이전 페이지로 이동
+            } else {
+                $boardTitle = $connect -> real_escape_string($boardTitle);
+                $boardContents = $connect -> real_escape_string($boardContents);
+    
+                $sql = "INSERT INTO board(memberID, boardTitle, boardContents, boardView, regTime) VALUES('$memberID', '$boardTitle', '$boardContents', '$boardView', '$regTime')";
+    
+                $connect -> query($sql);
+    
+                echo "<script>alert('게시글이 성공적으로 작성되었습니다.');</script>";
+                echo '<script>window.location.href = "board.php";</script>';
+            }
+        }
+    ?>
+</body>
+</html>
